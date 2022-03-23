978c4543fd9eed5f6f1000ab8dbe2845e1d1abbb
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-23 11:55:47 +0300

    added logic to server file

--- Client/index.js
+++ Client/index.js
@@ -2,7 +2,7 @@ document.querySelector('button').addEventListener('click', getDetails)
 
 async function getDetails(){
     try{
-        const res = await fetch('http://localhost:8000/api/people')
+        const res = await fetch('http://localhost:8000/api/people/id')
         const data = await res.json()
     }catch(err){
         console.log(err)
--- Server/server.js
+++ Server/server.js
@@ -9,24 +9,25 @@ const PORT = 8000
 
 let people = {
     'stephen':{
-
+         'id': 0,
         'age':28,
         'name':'stephen',
         'birthLocation':'nairobi'
     },
     'kihuni':{
-       
+        'id': 1,
         'age':30,
         'name':'kihuni',
         'birthLocation':'nairobi' 
     },
     'mike':{
-        
+         'id':2,
         'age':27,
         'name':'mike',
         'birthLocation':'nairobi'
     },
     'unknown':{
+        'id':3,
         'age': 'unknown',
         'name': 'unknown',
         'birthLocation':'nairobi' 
@@ -38,10 +39,10 @@ app.get('/', (req,res) =>{
  res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/people/:peopleName', (req,res)=>{
-    const peopleName = req.params.peopleName
-    console.log(peopleName)
-    res.json(people[peopleName])
+app.get('/api/people/:id', (req,res)=>{
+    const identity = req.params.id
+    const person = people.find(person => person.id == identity)
+    res.json(person)
 })
 
 app.listen(PORT, ()=>{
