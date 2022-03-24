bba4e3f6be4665462dda0ac518b93f00d2c8f976
Stephen kihuni <stephenkihuni55@gmail.com>
2022-03-24 07:08:41 +0300

    added logic to the server

--- Server/server.js
+++ Server/server.js
@@ -2,47 +2,69 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+app.use(express.json())
 app.use(cors())
 
 const PORT = 8000
 
 
-let people = {
-    'stephen':{
-         'id': 0,
-        'age':28,
-        'name':'stephen',
-        'birthLocation':'nairobi'
+let people = [
+    {
+        id: 0,
+        age:28,
+        name:'stephen',
+        birthLocation:'nairobi'
     },
-    'kihuni':{
-        'id': 1,
-        'age':30,
-        'name':'kihuni',
-        'birthLocation':'nairobi' 
+    {
+        id: 1,
+        age:30,
+        name:'kihuni',
+        birthLocation:'nairobi' 
     },
-    'mike':{
-         'id':2,
-        'age':27,
-        'name':'mike',
-        'birthLocation':'nairobi'
+    {
+         id:2,
+        age:27,
+        name:'mike',
+        birthLocation:'nairobi'
     },
-    'unknown':{
+    {
         'id':3,
-        'age': 'unknown',
-        'name': 'unknown',
+        'age': '40',
+        'name': 'manman',
         'birthLocation':'nairobi' 
     }
 
-}
+]
 
 app.get('/', (req,res) =>{
  res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/api/people/:id', (req,res)=>{
-    const identity = req.params.id
-    const person = people.find(person => person.id == identity)
+    const identity = Number(req.params.id)
+    console.log(identity)
+    const person = people.find(p => {
+        console.log(p.id === identity)})
+
+    if(person){
+        res.json(person)
+    }else {
+        res.status(404).end()
+    }
+    
+})
+
+app.post('/api/people', (req,res) =>{
+    const person = req.body
+    console.log(person)
     res.json(person)
+})
+
+app.delete('/api/people/:id', (req,res) =>{
+    const identity  = Number(req.params.id)
+    people = people.filter(p => p.id !== id)
+
+    res.status(204).end()
 })
 
 app.listen(PORT, ()=>{
